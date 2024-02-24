68096839d1e9f80b57dfb871f84ee8d1c9c65485
Divya <divyasarvepalli15@gmail.com>
2024-02-23 18:13:16 -0600

    Divya Sarvepalli: Created cocoa class & assigned to storyboard

---
+++ EasyEvents/EasyEvents/loginVC.swift
@@ -0,0 +1,29 @@
+//
+//  loginVC.swift
+//  EasyEvents
+//
+//  Created by Divya Sarvepalli on 2/23/24.
+//
+
+import UIKit
+
+class loginVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
