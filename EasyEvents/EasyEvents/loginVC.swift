c7b06679d9fc12919317a1db8048540f42521148
Rakesh Somanaboina <rakesh.raya9696@gmail.com>
2024-03-08 02:30:12 -0600

    Rakesh Somanaboina: Added the logic for password validation

--- EasyEvents/EasyEvents/loginVC.swift
+++ EasyEvents/EasyEvents/loginVC.swift
@@ -33,7 +33,7 @@ class loginVC: UIViewController {
     
     @IBAction func onLogin(_ sender: UIButton) {
         AudioServicesPlaySystemSound(1103)
-        if(validateEmail(email: emailTF.text)){
+        if(validateEmail(givenEmail: emailTF.text) && validatePassword(givenPassword: passwordTF.text)){
             // verify the users email and password with database datadf
             if(emailTF.text == "admin@gmail.com" && passwordTF.text == "password"){
                 // navigate to the main view after succesful login
@@ -47,15 +47,53 @@ class loginVC: UIViewController {
         AudioServicesPlaySystemSound(1103)
     }
     
-    func validateEmail(email :String?)-> Bool{
-        guard let email = emailTF.text, !email.isEmpty else {
+    func validateEmail(givenEmail :String?)-> Bool{
+        guard let email = givenEmail, !email.isEmpty else {
             self.emailValidationLBL.text = "Please enter the Email ID"
             self.emailValidationLBL.textColor = UIColor.red
             return false}
         let emailRegex = "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,64}"
            let emailPredicate = NSPredicate(format: "SELF MATCHES %@", emailRegex)
-        self.emailValidationLBL.text = "Please enter a valid email ID"
-           return emailPredicate.evaluate(with: email)
+        if(!emailPredicate.evaluate(with: email)){
+            self.emailValidationLBL.text = "Please enter a valid email ID"
+            self.emailValidationLBL.textColor = UIColor.red
+               return false
+        }
+        return true
+    }
+    
+    func validatePassword(givenPassword: String?) -> Bool {
+        guard let password = givenPassword , !password.isEmpty else {
+                    self.passwordValidationLBL.text = "Please enter the password"
+                    self.passwordValidationLBL.textColor = UIColor.red
+                    return false
+                }
+        var messages: [String] = []
+        messages.append("Password must contain at least")
+
+        if !password.contains(where: { $0.isUppercase }) {
+            messages.append(" one capital letter.")
+        }
+        
+        if !password.contains(where: { $0.isNumber }) {
+            messages.append(", one digit.")
+        }
+        
+        if !password.contains(where: { !$0.isLetter && !$0.isNumber }) {
+            messages.append(", special character.")
+        }
+
+        if password.count < 8 {
+            messages.append(", 8 characters long.")
+        }
+        
+        if(messages.count > 1){
+            self.passwordValidationLBL.text = messages.joined(separator: "")
+            self.passwordValidationLBL.textColor = UIColor.red
+            return false
+        }
+
+        return true
     }
     
     /*
