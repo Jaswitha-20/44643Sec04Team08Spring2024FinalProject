62877d8c29fe12fcde0fa16f437f7a4e5269a743
Rakesh Somanaboina <rakesh.raya9696@gmail.com>
2024-03-08 01:45:55 -0600

    Rakesh Somanaboina: Added labels for email, password validation and added logic for email validation

--- EasyEvents/EasyEvents/loginVC.swift
+++ EasyEvents/EasyEvents/loginVC.swift
@@ -8,13 +8,16 @@
 import UIKit
 import AnimatedGradientView
 import AVFoundation
+import SwiftUI
 
 class loginVC: UIViewController {
 
     @IBOutlet weak var emailTF: UITextField!
     @IBOutlet weak var passwordTF: UITextField!
     
+    @IBOutlet weak var emailValidationLBL: UILabel!
     
+    @IBOutlet weak var passwordValidationLBL: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         let animatedGradient = AnimatedGradientView(frame: view.bounds)
@@ -30,9 +33,29 @@ class loginVC: UIViewController {
     
     @IBAction func onLogin(_ sender: UIButton) {
         AudioServicesPlaySystemSound(1103)
+        if(validateEmail(email: emailTF.text)){
+            // verify the users email and password with database datadf
+            if(emailTF.text == "admin@gmail.com" && passwordTF.text == "password"){
+                // navigate to the main view after succesful login
+              //  self.performSegue(withIdentifier: loginNext, sender: self)
+            }
+        }
     }
+    
+    
     @IBAction func onSignUp(_ sender: UIButton) {
         AudioServicesPlaySystemSound(1103)
+    }
+    
+    func validateEmail(email :String?)-> Bool{
+        guard let email = emailTF.text, !email.isEmpty else {
+            self.emailValidationLBL.text = "Please enter the Email ID"
+            self.emailValidationLBL.textColor = UIColor.red
+            return false}
+        let emailRegex = "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,64}"
+           let emailPredicate = NSPredicate(format: "SELF MATCHES %@", emailRegex)
+        self.emailValidationLBL.text = "Please enter a valid email ID"
+           return emailPredicate.evaluate(with: email)
     }
     
     /*
