7e5a859df2014de5eea20b10f61a5fb2e2b8cdab
Divya <divyasarvepalli15@gmail.com>
2024-03-29 19:10:56 -0500

    Divya Sarvepalli : Added AnimatedgradientView to all ViewControllers

--- EasyEvents/EasyEvents/ViewControllers/ProfileVC.swift
+++ EasyEvents/EasyEvents/ViewControllers/ProfileVC.swift
@@ -6,11 +6,21 @@
 //
 
 import UIKit
+import AnimatedGradientView
 
 class ProfileVC: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        let animatedGradient = AnimatedGradientView(frame: view.bounds)
+                animatedGradient.direction = .up
+                animatedGradient.animationValues = [(colors: ["#A9F5F2", "#F5F6CE"], .up, .axial),
+                                                    (colors: ["#F5A9D0", "#2ECCFA", "#BEF781"], .right, .axial),
+                                                    (colors: ["#ECE0F8", "#819FF7"], .down, .axial),
+                                                    (colors: ["#58FAF4", "#F4FA58", "#A9A9F5"], .left, .axial)]
+                view.addSubview(animatedGradient)
+                view.sendSubviewToBack(animatedGradient)
 
         // Do any additional setup after loading the view.
     }
